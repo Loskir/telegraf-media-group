1301e1368d90f3786e46354bb061a0687807593d
Maksim <m_telega@mail.ru>
2022-04-23 19:13:38 +0300

    Update example files

---
+++ example-telegraf-v4.js
@@ -0,0 +1,18 @@
+const { Telegraf } = require('telegraf')
+
+const mediaGroup = require('.')
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+bot.use(mediaGroup())
+
+bot.on('message', async (ctx) => {
+  let i = 0
+  for (const message of ctx.mediaGroup) {
+    await ctx.reply(`hi! ${i}`, {reply_to_message_id: message.message_id})
+    i += 1
+  }
+  return ctx.reply(`total: ${ctx.mediaGroup.length}`)
+})
+
+bot.startPolling()
